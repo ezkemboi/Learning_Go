9f7e531812414aa8141d8bef7b9a5f219a3a44db
ezrqnkemboi <ezrqnkemboi@gmail.com>
2019-02-23 19:47:22 +0300

    feature(short-var-declaration): implement short variable declaration

---
+++ tour_go/Basics/packages-variables-functions/short-variables-declarations.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variables 
+	var i, j int = 23, 45
+
+	// short variables declarations 
+	k := 12
+	c, erlang, Javascript := true, false, "nope"
+	fmt.Println(i, j, k, c, erlang, Javascript)
+}
