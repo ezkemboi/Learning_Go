d1a396d4ab3bfca77326183a97c0ec9e960f387b
ezrqnkemboi <ezrqnkemboi@gmail.com>
2019-02-23 19:27:40 +0300

    feature(variable): naming and printing of variables

---
+++ tour_go/Basics/packages-variables-functions/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Get 3 variables of type boolean
+var c, python, javascript bool
+
+func main() {
+	// Get a variable i, which is of type integer
+	var i int
+	// Int is initialized with 0
+	fmt.Println(i, c, python, javascript)
+}
