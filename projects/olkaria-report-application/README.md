1fbd570c7455e25dba4d4864bfb5975b8d9198e6
ezrqnkemboi <ezrqnkemboi@gmail.com>
2019-02-25 08:10:18 +0300

    feature(report-app): start report app for olkaria

---
+++ projects/olkaria-report-application/README.md
@@ -0,0 +1,10 @@
+# Olkaria Report Application
+
+- This application is a report provider for olkaria company. It is a geothermal and energy
+provider. 
+- The report keeps track of the company's powerplant and how much demand the power grid 
+is exerting to those plants. 
+
+
+# Contributor 
+[Ezrqn Kemboi](https://github.com/ezrqnkemboi)
---
+++ projects/olkaria-report-application/main.go
@@ -0,0 +1,26 @@
+package main
+
+
+import "fmt"
+
+/**
+Simple report that calculates the current power capacity feeding feeding grid
+Compare it with current load and check on which it is utilized
+*/
+
+func main() {
+	var plantcapacities []float64
+
+	plantcapacities = []float64{30, 30, 30, 60, 60, 100}
+
+	var capacity float64 = plantcapacities[0] + plantcapacities[1] + plantcapacities[2] + 
+		plantcapacities[3] + plantcapacities[4] + plantcapacities[5]
+
+	var gridLoad = 75.
+
+	utilization := gridLoad / capacity
+
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "GridLoad: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", utilization * 100)
+}
